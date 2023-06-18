d390c6c72c29393194abda44d6acdd15a453381d
maki9789 <tajyccbjp@gmail.com>
2023-06-18 16:45:37 +0900

    #2 creationg welcome page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -6,7 +6,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
         <>
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                {/* <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
@@ -31,10 +31,44 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                             </Link>
                         </>
                     )}
-                </div>
+                </div> */}
 
                 <div className="max-w-7xl mx-auto p-6 lg:p-8">
-                    <div className="flex justify-center">
+                    <div className="text-center">
+                        <p className="text-white mb-4">週単位のお小遣い管理アプリ</p>
+                        <h1 className="text-white text-6xl mb-12">CHOMANE</h1>
+                    </div>
+                    <div className="text-center">
+                        {auth.user ? (
+                            <Link
+                                href={route('dashboard')}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Dashboard
+                            </Link>
+                        ) : (
+                            <>
+                                <Link
+                                    href={route('register')}
+                                    className="block bg-white rounded-full px-4 py-2 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                >
+                                    新規アカウント作成
+                                </Link>
+
+                                <Link
+                                        href={route('login')}
+                                        className="block mt-6 bg-white rounded-full px-4 py-2 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                    >
+                                        ログイン
+                                </Link>
+
+                            </>
+                        )}
+                    </div>
+
+
+
+                    {/* <div className="flex justify-center">
                         <svg
                             viewBox="0 0 62 65"
                             fill="none"
@@ -289,9 +323,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
-                    <div className="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
+                    {/* <div className="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
                         <div className="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                             <div className="flex items-center gap-4">
                                 <a
@@ -319,7 +353,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                         <div className="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                             Laravel v{laravelVersion} (PHP v{phpVersion})
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
